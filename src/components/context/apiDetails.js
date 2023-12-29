9709ebee0ff3ff1c05872332f1e033127081c213
Omotosho Kamalideen <dapious99@gmail.com>
2023-12-29 04:48:36 +0100

    tmdb details added

--- src/components/context/apiDetails.js
+++ src/components/context/apiDetails.js
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 const apiDetailsContext = createContext();
 
 const GlobalContext = ({ children }) => {
-  const API_KEY = "your_api_key";
+  const API_KEY = "c65746b544aa885476cc5360668dfd99";
   const BASE_URL = "https://api.themoviedb.org/3";
   return (
     <apiDetailsContext.Provider value={(API_KEY, BASE_URL)}>
---
+++ src/views/AboutUs/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import tmdblogo from "../../assets/tmdblogo.svg";
+
+const index = () => {
+  return (
+    <div>
+      {/*This block will be utilized when commercializing the project <div>
+        <img src={tmdblogo} />
+        <p>
+          "This [website, program, service, application, product] uses TMDB and
+          the TMDB APIs but is not endorsed, certified, or otherwise approved by
+          TMDB."
+        </p>
+  </div>*/}
+    </div>
+  );
+};
+
+export default index;
