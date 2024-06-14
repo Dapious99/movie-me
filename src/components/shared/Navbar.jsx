fd4a4764b040d9b172fa89ae6ad8d1d9d7c0e8a6
Omotosho Kamalideen <dapious99@gmail.com>
2024-06-14 11:40:23 +0100

    more functionality and a search area on navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { HiSearch } from "react-icons/hi";
 const Navbar = () => {
   return (
-    <div className="bg-orange-400 text-white font-sansSerif py-4 px-12 lg:px-5 md:px-4 sm:px-3 xs:px-2">
+    <div className="bg-orange-400 text-white font-sansSerif py-4 px-12 lg:px-5 md:px-4 sm:px-3 xs:px-2 flex justify-between">
       <Link to="/" className="italic text-2xl text-red-700">
         movieMe
       </Link>
+      <div>
+        <div className="bg-white">
+          <HiSearch />
+          <input placeholder="search your movie here" />
+        </div>
+      </div>
     </div>
   );
 };
